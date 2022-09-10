d3ed58d7282007f6ce60bb4825139327353207be
Dan H <danhoangftu@gmail.com>
2022-09-10 16:40:48 -0700

    cc

--- ui.R
+++ ui.R
@@ -42,7 +42,6 @@ shinyUI(fluidPage(
        tabPanel('Details', div(tableOutput('summary1'), tableOutput('summary2'))),
        tabPanel('Frequency Graph', plotOutput("graph1", width=1000, height=500)),
        tabPanel('Spectrogram', plotOutput("graph2", width=1000, height=500))
-     ),
     
   ))
-))
+))))
